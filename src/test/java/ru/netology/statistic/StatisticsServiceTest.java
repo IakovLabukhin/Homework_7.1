dcde66b07214191daa167e5fa6ffa2eba1256eca
Iakov Labukhin <zozzazzozzaz@gmail.com>
2024-06-12 16:14:36 +0300

    Full tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,41 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxWithSingleValue() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {5};
+        long expected = 5;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithNegativeValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {-10, -5, -8, -4, -5, -3};
+        long expected = -3;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithEqualValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {8, 8, 8, 8, 8, 8};
+        long expected = 8;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
